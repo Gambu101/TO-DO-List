cfd0d0ce44b17d987e995b9c1732d9d93b8b5f41
Ohalete Gabriel <100051823+Gambu101@users.noreply.github.com>
2024-09-23 12:04:07 +0100

    Add files via upload

---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import Form from "./Form";
+import TodoList from "./TodoList";
+import Footer from "./footer";
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo)=>todo.done).length
+  const totalTodos = todos.length
+  return (
+    <>
+      <Form todos = {todos} setTodos = {setTodos}/>
+      <TodoList todos = {todos} setTodos ={setTodos}/>
+      <Footer totalTodos = {totalTodos} completedTodos ={completedTodos}/>
+    </>
+  );
+}
